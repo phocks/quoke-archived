9f898ac8ae839c2aebccad00f38523c4cda17caa
Joshua Byrd <phocks@gmail.com>
2019-10-06 23:55:02 +1000

    Log in display

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,22 @@
 import Link from "next/link";
 import fetch from "isomorphic-unfetch";
 import absoluteUrl from "next-absolute-url";
-import nextCookie from 'next-cookies'
+import nextCookie from "next-cookies";
+import jwt from "jsonwebtoken";
 
 // Components
 import Layout from "../components/layout";
 import Heading from "../components/heading";
+
+const authState = token => {
+  if (!token) return { loggedIn: false };
+  try {
+    var decoded = jwt.verify(token, process.env.jwtSecret);
+    return { loggedIn: true, payload: decoded };
+  } catch (err) {
+    return { loggedIn: false, error: err };
+  }
+};
 
 const Home = props => (
   <Layout>
@@ -19,12 +30,13 @@ const Home = props => (
       </h1> */}
 
       <blockquote>{props.quote.text}</blockquote>
-      {/* <p>Logged in as {props.username}</p> */}
+      <p>Logged in: {props.loggedIn.toString()}</p>
+      <p>{props.username}</p>
     </div>
   </Layout>
 );
 
-Home.getInitialProps = async (ctx) => {
+Home.getInitialProps = async ctx => {
   const { req, query } = ctx;
   const { origin } = absoluteUrl(req);
   const apiOrigin = `${origin}/api/`;
@@ -32,11 +44,12 @@ Home.getInitialProps = async (ctx) => {
   const res = await fetch(apiOrigin + "random");
   const randomQuote = await res.json();
 
-  const { token } = nextCookie(ctx)
-  console.log(token)
-
+  const { token } = nextCookie(ctx);
+  const auth = authState(token);
 
   return {
+    loggedIn: auth.loggedIn,
+    username: auth.payload.username,
     quote: randomQuote
   };
 };
