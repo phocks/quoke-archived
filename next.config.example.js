bf071b3fdc6ceb097ce0030e19f770d85fab9032
Joshua Byrd <phocks@gmail.com>
2019-10-16 21:49:58 +1000

    Add sample config

---
+++ next.config.example.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    dbUser: "quoke",
+    dbPassword: "change me",
+    jwtSecret: "a long secret that only you know",
+    recaptchaSecret: "a reCAPTCHA secret token"
+  }
+};
