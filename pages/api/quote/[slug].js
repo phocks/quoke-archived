519b96014b55526d1616f059771dbb96b0899d5a
Joshua Byrd <phocks@gmail.com>
2019-10-19 23:00:09 +1000

    New slugs....

--- pages/api/quote/[slug].js
+++ pages/api/quote/[slug].js
@@ -7,7 +7,15 @@ export default async function handle(req, res) {
   const client = await MongoClient.connect(url, options);
   const db = client.db(dbName);
   const collection = db.collection("quotations");
-  const result = await collection.find({ _id: ObjectID(slug) }).toArray();
+
+  const searchString = "^" + slug.replace(/\-/g, ".*");
+
+  const regexSearch = new RegExp(searchString, "i");
+
+  console.log(regexSearch)
+  
+
+  const result = await collection.find({ text: regexSearch }).toArray();
 
   res.status(200).json(result);
   client.close();
--- pages/random.js
+++ pages/random.js
@@ -1,18 +1,25 @@
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 import absoluteUrl from "next-absolute-url";
+import slugify from "slugify";
 
 import Layout from "../components/layout";
+
+// Number of words to truncate the quote text to
+const SLUG_WORD_COUNT = 8;
 
 const Random = props => {
   const router = useRouter();
 
   useEffect(() => {
-    router.replace("/quote/[slug]", "/quote/" + props.randomQuote._id.toString());
+    const quoteText = props.randomQuote.text;
+    const truncatedText = truncate(quoteText, SLUG_WORD_COUNT);
+    const slugText = slugify(truncatedText, { lower: true });
+    router.replace("/quote/[slug]", "/quote/" + slugText);
   }, []);
   return (
     <Layout>
-      <main className="content">Loading...</main>
+      <main className="content"></main>
     </Layout>
   );
 };
@@ -32,3 +39,10 @@ Random.getInitialProps = async ctx => {
 };
 
 export default Random;
+
+function truncate(str, no_words) {
+  return str
+    .split(" ")
+    .splice(0, no_words)
+    .join(" ");
+}
