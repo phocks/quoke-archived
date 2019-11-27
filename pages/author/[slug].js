13b7bbc01546572fc12f1106881f2cdcf5d2eb46
Joshua Byrd <phocks@gmail.com>
2019-11-28 06:48:15 +1000

    Start of authors and put api get in lib

--- pages/author/[slug].js
+++ pages/author/[slug].js
@@ -1,26 +1,25 @@
 import { apiGet } from "../../lib/utils";
 
 import Layout from "../../components/layout";
-import Quotation from "../../components/quotation";
 import Title from "../../components/title";
 
 export default function Quote(props) {
   const { quote } = props;
   return (
-    <Layout title="A quote from Quoke">
-      <Title text="/quote" />
+    <Layout title="Author page - Quoke">
+      <Title text="/author" />
       <div className="spacer" />
       <hr />
-      <Quotation quote={quote} />
+      {props.data[0].name}
       <hr />
     </Layout>
   );
 }
 
 Quote.getInitialProps = async ({ req, query }) => {
-  const data = await apiGet(req, "/api/quote/" + query.slug);
+  const data = await apiGet(req, "/api/main");
 
   return {
-    quote: data
+    data: data
   };
 };
