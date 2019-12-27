61c5ed61bfae97d65c7e41e9181f1fbcee96093e
Joshua Byrd <phocks@gmail.com>
2019-12-28 09:08:32 +1000

    Breakpoints

--- components/heading.js
+++ components/heading.js
@@ -7,7 +7,7 @@ const Heading = props => {
       <div className={css.branding}>
         <Link href="/">
           <a>
-            <img src="/img/right-quote.svg" alt="Quoke" />
+            <img src="/img/left-quote.svg" alt="Quoke" />
           </a>
         </Link>
       </div>
