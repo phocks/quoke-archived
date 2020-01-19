67ba6bc2450dd0f4e0f9a666d0787190e8ebc7de
Joshua Byrd <phocks@gmail.com>
2020-01-19 14:13:08 +1000

    Small

--- components/quotation.js
+++ components/quotation.js
@@ -8,30 +8,12 @@ export default props => {
     <div className={css.root}>
       <blockquote cite="https://quoke.co">
         <span>&ldquo;{smartquotes(quote.text)}&rdquo;</span>
-
-        <span>
+        <small>
           {" "}
           &mdash;{quote.author}
           {quote.source && <cite>, {quote.source}</cite>}
-        </span>
+        </small>
       </blockquote>
     </div>
-    // <div className={css.root}>
-    //   <div className={css.container}>
-    //     <div className={css.quote}>
-    //       <div className={css.text}>{smartquotes(quote.text)}</div>
-    //       <div className={css.citationContainer}>
-    //         <div className={css.citation}>
-    //           <span className={css.author}>
-    //             &mdash;{quote.author}
-    //             {quote.source && (
-    //               <em className={css.source}>, {quote.source}</em>
-    //             )}
-    //           </span>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-    // </div>
   );
 };
