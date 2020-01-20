41be66da19ff5dff0302e20fbf0374531bc7e63b
Joshua Byrd <phocks@gmail.com>
2020-01-20 16:05:18 +1000

    Pagination basic

--- pages/api/get-quotes/index.js
+++ pages/api/get-quotes/index.js
@@ -1,7 +1,6 @@
 import { findHome } from "../../../lib/mongodb";
 
 export default async (req, res) => {
-  console.log(req.query);
   const { limit, skip } = req.query;
 
   const quotes = await findHome(
--- pages/page/[page].js
+++ pages/page/[page].js
@@ -6,18 +6,21 @@ import dayjs from "dayjs";
 import { useStoreState, useStoreActions } from "easy-peasy";
 import absoluteUrl from "next-absolute-url";
 import smartquotes from "smartquotes";
+import { useRouter } from "next/router";
 
-import { apiGet, truncate } from "../lib/utils";
+import { apiGet, truncate } from "../../lib/utils";
 
-import Layout from "../components/layout";
-import Quotation from "../components/quotation";
+import Layout from "../../components/layout";
+// import Quotation from "../components/quotation";
 
-import css from "./index.scss";
+import css from "../index.scss";
 
 let cache = null;
 
-const Home = props => {
+const Pagination = props => {
   const { quotes } = props.data;
+  const router = useRouter();
+  const { page } = router.query;
 
   if (process.browser) cache = props.data;
 
@@ -31,7 +34,17 @@ const Home = props => {
         ))}
 
         <div>
-          <Link href="/page/[page]" as="/page/2">
+          {page === "2" ? (
+            <Link href="/">
+              <a>&lt; Previous</a>
+            </Link>
+          ) : (
+            <Link href="/page/[page]" as={`/page/${+page - 1}`}>
+              <a>&lt; Previous</a>
+            </Link>
+          )}
+          &nbsp;/&nbsp;
+          <Link href="/page/[page]" as={`/page/${+page + 1}`}>
             <a>Next &gt;</a>
           </Link>
         </div>
@@ -40,13 +53,15 @@ const Home = props => {
   );
 };
 
-Home.getInitialProps = async ({ req, query }) => {
+Pagination.getInitialProps = async ({ req, query }) => {
+  let { page } = query;
   let data = {};
 
   const quotesPerPage = 5;
-  let skip = 0;
+  const skipMultiplier = +page - 1;
+  let skip = skipMultiplier * quotesPerPage;
 
-  if (cache) data = cache;
+  if (false) data = cache;
   else
     data.quotes = await apiGet(
       req,
@@ -56,4 +71,4 @@ Home.getInitialProps = async ({ req, query }) => {
   return { data: data };
 };
 
-export default Home;
+export default Pagination;
