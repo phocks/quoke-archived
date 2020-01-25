9d204fe8e9e12e447b2a7523b1286787e2fe9968
Joshua Byrd <phocks@gmail.com>
2020-01-26 08:03:53 +1000

    Get rid of google

--- components/layout.js
+++ components/layout.js
@@ -17,14 +17,9 @@ const Layout = props => {
           content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
         /> */}
         <meta name="viewport" content="width=768"></meta>
-        {/* <link rel="icon" href="/favicon.ico" /> */}
+        <link rel="icon" href="/favicon.ico" />
         <link rel="icon" type="image/png" href="/icons/favorite-7-240.png" />
         <link rel="stylesheet" href="/css/normalize.css"></link>
-        <script
-          data-ad-client="ca-pub-4754239438008393"
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"
-        ></script>
       </Head>
 
       <div className={css.container}>
@@ -37,7 +32,6 @@ const Layout = props => {
           <Foot />
         </footer>
       </div>
-      <script> </script>
     </>
   );
 };
