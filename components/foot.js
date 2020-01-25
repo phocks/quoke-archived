8e3c0fd32b039a058a6d9a0146e6b55ae9475477
Joshua Byrd <phocks@gmail.com>
2020-01-25 11:39:08 +1000

    Search starting

--- components/foot.js
+++ components/foot.js
@@ -3,8 +3,8 @@ import css from "./foot.scss";
 
 export default props => {
   return (
-    <div className={css.root}>
-      About
+    <div className={css.foot}>
+      <Link href="/"><a>Home</a></Link> <Link href="/about"><a>About</a></Link>
     </div>
   );
 };
