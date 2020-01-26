d9aefd3946f3f8a265176c934b81dc68a76d3bca
Joshua Byrd <phocks@gmail.com>
2020-01-26 19:05:46 +1000

    Search features

--- pages/search.js
+++ pages/search.js
@@ -1,13 +1,13 @@
 import Layout from "../components/layout";
-import css from "./about.scss";
+import css from "./search.scss";
 
 const Search = props => {
   return (
     <Layout title={"About — Quoke"}>
-      <div className={css.about}>
+      <div className={css.search}>
         <div className={css.top}>
           <h1 className={css.title}>Results</h1>
-          <p>here are your results</p>
+          <p>here are your results (search feature coming soon...)</p>
         </div>
       </div>
       <hr />
