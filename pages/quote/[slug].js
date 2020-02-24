30206b2334076f9221f3e6c74e3f113d825f5c5b
Joshua Byrd <phocks@gmail.com>
2020-02-24 15:19:17 +1000

    Increment 1 each view

--- pages/quote/[slug].js
+++ pages/quote/[slug].js
@@ -1,18 +1,22 @@
 import { apiGet, truncate } from "../../lib/utils";
 
 import Layout from "../../components/layout";
-import Quote from "../../components/quote"
-
-import css from "./[slug].scss";
+import Quote from "../../components/quote";
 
 export default function Quotation(props) {
   const { quote } = props.data;
 
   return (
     <Layout title={(quote.title || truncate(quote.text, 5)) + " — Quoke"}>
-      <div className={`${css.root} col`}>
+      <div className={`root col`}>
         <Quote quote={quote} />
       </div>
+      <style jsx>{`
+        .root {
+          padding-top: 0.5em;
+          padding-bottom: 0.5em;
+        }
+      `}</style>
     </Layout>
   );
 }
